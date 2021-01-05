d3fd0d4c7573ac10afcb949d8afc1eee0527297f
Lucio De Simone <lucio.desimone@globant.com>
2021-01-05 14:10:02 -0300

    [BGO-2] - Implementing a generic interface for the in-memory database

--- db/db.go
+++ db/db.go
@@ -1,7 +1,58 @@
 package db
 
 //db global storage in memory
-var db = make(map[int64]Beer)
+var db = make(map[int64]interface{})
 
 //id auto incremental value
 var index int64 = 0
+
+//Entity is an interface that allows to interact with the database
+type Entity struct{}
+
+//Create a new record in the database
+func (e Entity) Create(ent interface{}) interface{} {
+	index++
+	db[index] = ent
+	return ent
+}
+
+//Update a record by id in the database
+func (e Entity) Update(ent interface{}, id int64) bool {
+	_, ok := db[id]
+
+	if !ok {
+		return ok
+	}
+
+	db[id] = ent
+
+	return ok
+}
+
+//Get a record by id in the database
+func (e Entity) Get(id int64) interface{} {
+	ent, ok := db[id]
+
+	if !ok {
+		return Entity{}
+	}
+
+	return ent
+}
+
+//Delete a record by id in the database
+func (e Entity) Delete(id int64) bool {
+	_, ok := db[id]
+
+	if ok {
+		delete(db, id)
+	}
+
+	return ok
+}
+
+//Connection emulates returning a real connection to the database
+func Connection() *Entity {
+	//emulates a connection
+	return &Entity{}
+}
--- main.go
+++ main.go
@@ -30,18 +30,20 @@ func main() {
 		AvgScore:    86,
 	}
 
-	s := db.Create(stout)
-	bi := db.Create(bitter)
-	ba := db.Create(barley)
+	c := db.Connection()
+
+	s := c.Create(stout)
+	bi := c.Create(bitter)
+	ba := c.Create(barley)
 
 	log.Printf("Created: %v", s)
 	log.Printf("Created: %v", bi)
 	log.Printf("Created: %v", ba)
 
-	s = db.Get(1)
-	bi = db.Get(2)
-	ba = db.Get(3)
-	nf := db.Get(4)
+	s = c.Get(1)
+	bi = c.Get(2)
+	ba = c.Get(3)
+	nf := c.Get(4)
 
 	log.Printf("Found: %v", s)
 	log.Printf("Found: %v", bi)
@@ -53,15 +55,15 @@ func main() {
 	log.Printf("Found: %v", ba)
 	log.Printf("Not found: %v", nf)
 
-	del := db.Delete(2)
+	del := c.Delete(2)
 
 	log.Printf("Deleted: %v", del)
 
-	del = db.Delete(7)
+	del = c.Delete(7)
 
 	log.Printf("Deleted: %v", del)
 
-	bi = db.Get(2)
+	bi = c.Get(2)
 
 	log.Printf("Not found: %v", bi)
 
@@ -73,19 +75,19 @@ func main() {
 		AvgScore:    78,
 	}
 
-	upd := db.Update(creamStout, 1)
+	upd := c.Update(creamStout, 1)
 
 	log.Printf("Updated: %v", upd)
 
-	upd = db.Update(creamStout, 2)
+	upd = c.Update(creamStout, 2)
 
 	log.Printf("Updated: %v", upd)
 
-	cs := db.Get(1)
+	cs := c.Get(1)
 
 	log.Printf("Changed: %v", cs)
 
-	cs = db.Get(2)
+	cs = c.Get(2)
 
 	log.Printf("Not changed: %v", cs)
 }
